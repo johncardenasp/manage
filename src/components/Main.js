b996835564857f6318ca2339601212536e5c04b9
John Cardenas <jhoncardenaspinto@gmail.com>
2021-01-25 23:55:50 -0500

    Desktop styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -22,7 +22,7 @@ function Main() {
           alt="Intro" 
         />
       </div>
-      <div className="main__content">
+      <section className="better__products">
         <h1>
           Bring everyone together to build better products.
         </h1>
@@ -30,31 +30,35 @@ function Main() {
             Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goals in view.
         </p>
         <Button />
-      </div>
-      <section className="main__whats-different">
-        <h2>What’s different about Manage?</h2>
-        <p>
-          Manage provides all the functionality your team needs, without the complexity. Our software is tailor-made for modern digital product teams. 
-        </p>
-        <Card 
-          number="01"
-          title="Track company-wide progress"
-          description="See how your day-to-day tasks fit into the wider vision. Go from tracking progress at the milestone level all the way done to the smallest of details. Never lose sight of the bigger picture again."
-        />
-        <Card 
-          number="02"
-          title="Advanced built-in reports"
-          description="Set internal delivery estimates and track progress toward company goals. Our customisable dashboard helps you build out the reports you need to keep key stakeholders informed."
-        />
-        <Card 
-          number="03"
-          title="Everything you need in one place"
-          description="Stop jumping from one service to another to communicate, store files, track tasks and share documents. Manage offers an all-in-one team productivity solution."
-        />
       </section>
-      <div className="opinions__title">
-        <h2>What they’ve said</h2>
-      </div>
+      <section className="main__whats-different">
+        <div className="intro">
+          <h2>What’s different about Manage?</h2>
+          <p>
+            Manage provides all the functionality your team needs, without the complexity. Our software is tailor-made for modern digital product teams. 
+          </p>
+        </div>
+        <div className="cards">
+          <Card 
+            number="01"
+            title="Track company-wide progress"
+            description="See how your day-to-day tasks fit into the wider vision. Go from tracking progress at the milestone level all the way done to the smallest of details. Never lose sight of the bigger picture again."
+          />
+          <Card 
+            number="02"
+            title="Advanced built-in reports"
+            description="Set internal delivery estimates and track progress toward company goals. Our customisable dashboard helps you build out the reports you need to keep key stakeholders informed."
+          />
+          <Card 
+            number="03"
+            title="Everything you need in one place"
+            description="Stop jumping from one service to another to communicate, store files, track tasks and share documents. Manage offers an all-in-one team productivity solution."
+          />
+        </div>
+      </section>
+      <h2 className="opinions__title">
+        What they’ve said
+      </h2>
       <section className="opinions">
         <Opinion 
           image={anisha}
@@ -77,16 +81,16 @@ function Main() {
           blockquote="Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive."
         />
       </section>
-      <div className="move-slider">
+      <div className="move__slider">
         <div className="circles">
           <div className="circle" />
           <div className="circle" />
           <div className="circle" />
           <div className="circle" />
         </div>
-        <div className="circles__button">
-          <Button />
-        </div>
+       </div>
+      <div className="button__main-container">
+        <Button />
       </div>
       <section className="simplify__work">
         <div className="simplify__work-content">
